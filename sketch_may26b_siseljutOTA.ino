056285678f557de23baa7a2d7005655ef19d5ca8
Muhammad Lucky Harvi <lharvi@outlook.com>
2025-05-26 10:25:33 +0700

    Update sketch_may26b_siseljutOTA.ino

--- sketch_may26b_siseljutOTA.ino
+++ sketch_may26b_siseljutOTA.ino
@@ -17,13 +17,13 @@
 #define SCK  D1  
 
 // Wi-Fi and MQTT credentials
-const char* ssid = "SISELJUT";
-const char* password = "setubabakan";
+const char* ssid = "";
+const char* password = "";
 
 // Emqx
-const char* mqtt_server = "f225b61b.ala.asia-southeast1.emqxsl.com";
-const char* mqtt_user = "luckyharvi";
-const char* mqtt_pass = "12345";
+const char* mqtt_server = "....emqxsl.com";
+const char* mqtt_user = "";
+const char* mqtt_pass = "";
 
 // NTP Server settings
 const char *ntp_server = "pool.ntp.org";     // Default NTP server
